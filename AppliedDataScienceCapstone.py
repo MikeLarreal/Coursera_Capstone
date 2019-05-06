c4bddbe675dbfbe9a3023efe1daaa87dad96fa89
Miguel Larreal <mlarrealbaez@gmail.com>
2019-05-06 19:24:07 -0300

    Added code
    Added code

---
+++ AppliedDataScienceCapstone.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # This notebook will be mainly used through all the 
+# # Applied Data Science Capstone course
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone Project Course!")
+
+
+# In[ ]:
+
+
+
+
